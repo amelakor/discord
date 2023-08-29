e9d63ec68ed3d771083ff4957e71d43009d64a2e
amelakor <amela_korjenic@hotmail.com>
2023-08-29 14:25:12 +0200

    Server creation API

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 import {
     Dialog,
@@ -36,6 +38,8 @@ const formSchema = z.object({
 const InitialModal = () => {
     const [isMounted, setIsMounted] = useState(false);
 
+    const router = useRouter();
+
     useEffect(() => {
         setIsMounted(true);
     }, []);
@@ -50,7 +54,14 @@ const InitialModal = () => {
     const isLoading = form.formState.isSubmitting;
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values);
+        try {
+            await axios.post("/api/servers", values);
+            form.reset();
+            router.refresh();
+            window.location.reload();
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     if (!isMounted) {
