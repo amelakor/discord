791e818eaae1b9884c63ba69bd26a93a4c90dfef
amelakor <amela_korjenic@hotmail.com>
2023-08-30 14:29:44 +0200

    Channel

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,12 @@
 import { redirectToSignIn } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { ChannelType } from "@prisma/client";
 
 import { currentProfile } from "@/lib/current-profile";
+import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
+import ChatMessages from "@/components/chat/chat-messages";
+import { MediaRoom } from "@/components/media-room";
 import db from "@/lib/db";
 
 interface ChannelIdPageProps {
@@ -37,7 +42,31 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
 
     return (
         <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
-            Channel
+            <ChatHeader
+                name={channel.name}
+                serverId={channel.serverId}
+                type="channel"
+            />
+            {channel.type === ChannelType.TEXT && (
+                <>
+                    <ChatMessages />
+                    <ChatInput
+                        name={channel.name}
+                        type="channel"
+                        apiUrl="/api/socket/messages"
+                        query={{
+                            channelId: channel.id,
+                            serverId: channel.serverId,
+                        }}
+                    />
+                </>
+            )}
+            {channel.type === ChannelType.AUDIO && (
+                <MediaRoom chatId={channel.id} video={false} audio={true} />
+            )}
+            {channel.type === ChannelType.VIDEO && (
+                <MediaRoom chatId={channel.id} video={true} audio={true} />
+            )}
         </div>
     );
 };
