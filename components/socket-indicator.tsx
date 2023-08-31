961a8ddec1dc47e8bfe1f34f6496bd5243e44ee0
amelakor <amela_korjenic@hotmail.com>
2023-08-31 12:02:30 +0200

    Socket indicatore

--- components/socket-indicator.tsx
+++ components/socket-indicator.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { Badge } from "@/components/ui/badge";
+import { useSocket } from "@/components/providers/socket-provider";
 
 export const SocketIndicator = () => {
-    const isConnected = true;
+    const { isConnected } = useSocket();
 
     if (!isConnected) {
         return (
